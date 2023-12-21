176db60fe5e435960bf85edc97e15fb6c9a71466
tianywan819 <57418894+tianywan819@users.noreply.github.com>
2023-12-21 10:24:09 -0500

    Working Formik. Need to debug SAVE button not able to choose prefix

--- client/src/layouts/ObjectViewLayout.js
+++ client/src/layouts/ObjectViewLayout.js
@@ -3,20 +3,48 @@ import { Outlet } from "react-router-dom";
 import ObjectNavBar from "./ObjectNavBar";
 import ObjectSideBar from "./objectSideBar";
 import "../App.css";
+import SaveDraftButton from "../components/builder/SaveDraftButton";
 
-export default function ObjectLayout ()  {
+// export default function ObjectLayout ()  {
+//   const [domain, setDomain] = React.useState(0);
+//   return (
+//     <div className="outlet-root">
+//       <ObjectNavBar />
+//       <div className="outlet-container">
+//         <ObjectSideBar
+//           className="object-sidebar"
+//           domain={domain}
+//           setDomain={setDomain}
+//         />
+//         <SaveDraftButton/>
+//         <div className="outlet-content">
+//           <Outlet context={{domain, setDomain}}/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+export default function ObjectLayout() {
   const [domain, setDomain] = React.useState(0);
+
   return (
     <div className="outlet-root">
       <ObjectNavBar />
       <div className="outlet-container">
-        <ObjectSideBar
-          className="object-sidebar"
-          domain={domain}
-          setDomain={setDomain}
-        />
+        <div className="sidebar-and-button">
+          <div className="object-sidebar-container">
+            <ObjectSideBar
+              className="object-sidebar"
+              domain={domain}
+              setDomain={setDomain}
+            />
+          </div>
+          <div className="save-draft-button-container">
+            <SaveDraftButton />
+          </div>
+        </div>
         <div className="outlet-content">
-          <Outlet context={{domain, setDomain}}/>
+          <Outlet context={{ domain, setDomain }} />
         </div>
       </div>
     </div>
